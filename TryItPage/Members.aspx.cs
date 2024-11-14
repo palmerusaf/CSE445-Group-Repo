08de4123fb3e6adda6dc8cbef53ad9b6da752836
branden <brandenpalmer08@gmail.com>
2024-11-14 19:59:22 +0700

    add cookie cache for members page

--- TryItPage/Members.aspx.cs
+++ TryItPage/Members.aspx.cs
@@ -13,7 +13,13 @@ namespace web_client
         private readonly CurrencyExchangeClient serviceClient = new CurrencyExchangeClient();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            // get cache and set form 
+            HttpCookie exchangeRateData=Request.Cookies["exchangeRateData"];
+            if (exchangeRateData != null)
+            {
+                Currency1TextBox.Text = exchangeRateData["currency1"];
+                Currency2TextBox.Text = exchangeRateData["currency2"];
+            }
         }
 
         protected async void GetExchangeRateButton_Click(object sender, EventArgs e)
@@ -21,7 +27,11 @@ namespace web_client
             string currency1 = Currency1TextBox.Text.Trim();
             string currency2 = Currency2TextBox.Text.Trim();
 
-            
+            // remember input by caching into cookies
+            HttpCookie exchangeRateData = new HttpCookie("exchangeRateData");
+            exchangeRateData["currency1"]=currency1;
+            exchangeRateData["currency2"]=currency2;
+            Response.Cookies.Add(exchangeRateData);
 
             // Call the service method and get the exchange rate
             string result = await serviceClient.ExchangeRateAsync(currency1, currency2);
