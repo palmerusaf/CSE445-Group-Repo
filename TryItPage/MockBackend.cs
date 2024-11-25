29b14ed8ac43cd116c94a8a0351f30cbdadf697c
branden <brandenpalmer08@gmail.com>
2024-11-25 21:50:06 +0700

    add mock backend and validation

---
+++ TryItPage/MockBackend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MockBackendNameSpace
+{
+    public class SimpleResponse
+    {
+        public bool Success;
+        public string ErrorMsg;
+    }
+    public class ContentResponse<T>
+    {
+        public bool Success;
+        public string ErrorMsg;
+        public T Content;
+    }
+    public static class Backend
+    {
+        public static SimpleResponse ValidateLogin(string username, string password)
+        {
+            SimpleResponse res = new SimpleResponse();
+            // hash password bump with db
+            res.Success = true;
+            // res.Success = false;
+            // res.ErrorMsg="reason its invalid"
+            return res;
+        }
+    }
+}
