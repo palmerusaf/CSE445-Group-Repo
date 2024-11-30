05230d3bf7a19f2cdf4fb1cf47ce1898c8e23802
GavinCorrie <157530278+GavinCorrie@users.noreply.github.com>
2024-11-29 21:05:01 -0700

    Started Backend.

--- TryItPage/Backend.cs
+++ TryItPage/Backend.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Web;
+using System.Xml.Linq;
+using web_client.PasswordHasher;
 
 namespace BackendNameSpace
 {
+
+    public static class XmlHelper
+    {
+        private static readonly string XmlFilePath = HttpContext.Current.Server.MapPath("~\\TryItPage\\Users.xml");
+
+        public static XElement LoadXml()
+        {
+            return XElement.Load(XmlFilePath);
+        }
+        public static void SaveXml(XElement xml)
+        {
+            xml.Save(XmlFilePath);
+        }
+        public static XElement FindUser(string username)
+        {
+            XElement xml = LoadXml();
+            return xml.Elements("User").FirstOrDefault(u => u.Element("Username").Value == username);
+        }
+    }
+
+
+
+
+
     public class SimpleResponse
     {
         public bool Success;
@@ -16,24 +43,87 @@ namespace BackendNameSpace
         public string ErrorMsg;
         public List<string> Content;
     }
-    public static class Backend
+    public class Backend
     {
-
-        public static SimpleResponse ValidateLogin(string username, string password)
+        private IPasswordHasher client;
+        public SimpleResponse ValidateLogin(string username, string password)
         {
             SimpleResponse res = new SimpleResponse();
+            XElement user = XmlHelper.FindUser(username);
+
+            if (user == null)
+            {
+                res.Success = false;
+                res.ErrorMsg = "User does not exist.";
+                return res;
+            }
+
+            string storedHash = user.Element("PasswordHash").Value;
+            string salt = user.Element("Salt").Value;
+            if (client == null)
+            {
+                var factory = new ChannelFactory<IPasswordHasher>("BasicHttpBinding_IPasswordHasher");
+                client = factory.CreateChannel();
+            }
             // hash password bump with db
-            res.Success = true;
+            bool isValid = client.VerifyPassword(password, storedHash, salt);
+            if (isValid)
+            {
+
+                res.Success = true;
+                return res;
+            }
+            else
+            {
+                res.Success = false;
+                res.ErrorMsg = "Invalid password.";
+                return res;
+
+            }
+            //res.Success = true;
             // res.Success = false;
             // res.ErrorMsg="reason its invalid"
-            return res;
+            //return res;
         }
 
-        public static SimpleResponse CreateUser(string username, string password)
+        public SimpleResponse CreateUser(string username, string password)
         {
             // lets require usernames be unique so we can also use them as username ids
             // this means we'll have to bump the db to make sure this is true
             SimpleResponse res = new SimpleResponse();
+            XElement xml = XmlHelper.LoadXml();
+
+            if (XmlHelper.FindUser(username) != null)
+            {
+                res.Success = false;
+                res.ErrorMsg = "Username already exists.";
+                return res;
+            }
+
+            if (client == null)
+            {
+                var factory = new ChannelFactory<IPasswordHasher>("BasicHttpBinding_IPasswordHasher");
+                client = factory.CreateChannel();
+                string salt = client.GenerateSalt();
+                string NewHash = client.HashPassword(password, salt);
+
+
+                XElement newUser = new XElement("User",
+                new XElement("Username", username),
+                new XElement("PasswordHash", NewHash),
+                new XElement("Salt", salt)
+                );
+
+                xml.Add(newUser);
+                XmlHelper.SaveXml(xml);
+
+                res.Success = true;
+                return res;
+
+
+            }
+
+
             res.Success = true;
             // res.Success = false;
             // res.ErrorMsg="duplicate user"
